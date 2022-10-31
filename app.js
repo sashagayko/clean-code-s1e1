de806335d2ac6ce856f08f1a241fc456c4864889
Sasha <hlupik69@gmail.com>
2022-11-01 01:58:00 +0300

    refactor: add BEM according to rule 1.3

--- app.js
+++ app.js
@@ -50,7 +50,7 @@ var createNewTaskElement=function(taskString){
     deleteButton.className="btn delete";
     
     deleteButtonImg.src='./remove.svg';
-    deleteButtonImg.className="delete-img";
+    deleteButtonImg.className="btn-delete__img";
     deleteButton.appendChild(deleteButtonImg);
 
 
